788a04bdd4c517c677f078b7ca68f56547dba6d8
yashrajillusion <95868808+yashrajillusion@users.noreply.github.com>
2022-06-05 19:36:12 +0530

    Initiated a project of uber clone

---
+++ uber-clone/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView, Text, View } from "react-native";
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Lets build Uber</Text>
+    </SafeAreaView>
+  );
+}
